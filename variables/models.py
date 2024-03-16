9ed7031cb90151b5e091822503d5bdb9c2c1ef64
fpungo <60239261+fpungo@users.noreply.github.com>
2024-03-16 13:49:35 -0500

    cambios Logica

--- variables/models.py
+++ variables/models.py
@@ -13,5 +13,6 @@ class Variable(models.Model):
     monthly_income = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return '{}'.format(self.name)
+        return '{}'.format(self.name,self.first_name,self.last_name,self.document_type,self.document_number,self.date_of_birth,
+                           self.city_of_residence,self.email,self.phone_number,self.monthly_income)
 
