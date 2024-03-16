3fbad644aa6e79ab41a3db78f6879e95da621bd3
lojedaa <l.ojedaa@uniandes.edu.co>
2024-03-15 23:38:52 -0500

    modificacion forms models y templates

--- variables/forms.py
+++ variables/forms.py
@@ -6,7 +6,25 @@ class VariableForm(forms.ModelForm):
         model = Variable
         fields = [
             'name',
+            'first_name',
+            'last_name',
+            'document_type',
+            'document_number',
+            'date_of_birth',
+            'city_of_residence',
+            'email',
+            'phone_number',
+            'monthly_income',
         ]
         labels = {
             'name': 'Name',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'document_type': 'Document Type',
+            'document_number': 'Document Number',
+            'date_of_birth': 'Date of Birth',
+            'city_of_residence': 'City of Residence',
+            'email': 'Email',
+            'phone_number': 'Phone Number',
+            'monthly_income': 'Monthly Income',
         }
--- variables/models.py
+++ variables/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 class Variable(models.Model):
     name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    document_type = models.CharField(max_length=50)
+    document_number = models.IntegerField() 
+    date_of_birth = models.DateField()
+    city_of_residence = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    monthly_income = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return '{}'.format(self.name)
